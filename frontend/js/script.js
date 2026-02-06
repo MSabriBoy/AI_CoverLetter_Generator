707f4991cc0df3da26e95e75fe94bc391b772d8a
Rihan Sabri <mrihansabri@gmail.com>
2026-02-06 07:47:07 +0530

    Script file has corrected in fetch path

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -74,7 +74,7 @@ generateBtn.classList.add("loading");
 
 
 try{
-const res = await fetch("http://localhost:5000/api/generate", {
+const res = await fetch("/api/generate", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
